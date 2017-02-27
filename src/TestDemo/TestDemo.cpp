bfcbcb2ddf41e7b8d3cd5b63a7b3b915ec8368fc
5455945 <5455945@gmail.com>
2017-02-27 12:41:57 +0800

    #0003 zhangfj 添加catch单元测试库

--- src/TestDemo/TestDemo.cpp
+++ src/TestDemo/TestDemo.cpp
@@ -1,8 +1,61 @@
-#include <cstdio>
-#include <iostream>
+//#include <cstdio>
+//#include <iostream>
 
-int main(int argc, char** argv)
-{
-	std::cout << "hello world!" << std::endl;
-	return 0;
+//int main(int argc, char** argv)
+//{
+//	std::cout << "hello world!" << std::endl;
+//	return 0;
+//}
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+unsigned int Factorial(unsigned int number) {
+	std::cout << "number: " << number << std::endl;
+	return number <= 1 ? number : Factorial(number - 1)*number;
+}
+
+
+TEST_CASE("vectors can be sized and resized", "[vector]") {
+
+	std::vector<int> v(5);
+
+	REQUIRE(v.size() == 5);
+	REQUIRE(v.capacity() >= 5);
+
+	SECTION("resizing bigger changes size and capacity") {
+		v.resize(10);
+
+		REQUIRE(v.size() == 10);
+		REQUIRE(v.capacity() >= 10);
+	}
+	SECTION("resizing smaller changes size but not capacity") {
+		v.resize(0);
+
+		REQUIRE(v.size() == 0);
+		REQUIRE(v.capacity() >= 5);
+	}
+	SECTION("reserving bigger changes capacity but not size") {
+		v.reserve(10);
+
+		REQUIRE(v.size() == 5);
+		REQUIRE(v.capacity() >= 10);
+	}
+	SECTION("reserving smaller does not change size or capacity") {
+		v.reserve(0);
+
+		REQUIRE(v.size() == 5);
+		REQUIRE(v.capacity() >= 5);
+	}
+}
+
+TEST_CASE("Factorials are computed", "[factorial]") {
+	REQUIRE(Factorial(1) == 1);
+	REQUIRE(Factorial(2) == 2);
+	REQUIRE(Factorial(3) == 6);
+	REQUIRE(Factorial(10) == 3628800);
+}
+
+TEST_CASE("End", "[end]") {
+	REQUIRE(Factorial(1) == 1);
 }
